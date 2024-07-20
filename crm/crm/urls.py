943939a275a7b9d91bcff6b7cb30e01847e45dbd
Yaroslav D <dmiyaroslav92@gmail.com>
2024-07-20 17:09:40 -0600

    Set up static files path. Added products app path

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("users.urls")),
     path("admin/", admin.site.urls),
+    path("products/", include("products.urls")),
 ]
