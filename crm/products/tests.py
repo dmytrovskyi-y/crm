fce515a06e8a7dd65515ba6fbd611f6a596efbb6
Yaroslav D <dmiyaroslav92@gmail.com>
2024-07-26 18:44:56 -0600

    Added Products app tests and added new tests to Users app

--- crm/products/tests.py
+++ crm/products/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+from django.template.response import TemplateResponse
+from django.test import TestCase, Client
+from django.urls import reverse_lazy
 
-# Create your tests here.
+from products.models import Product
+
+
+
+class ProductModelTestCase(TestCase):
+    """
+    App Model Tests Products
+    """
+    @classmethod
+    def setUpTestData(cls):
+        ...
+
+
+class ProductViewTestCase(TestCase):
+    """
+    Tests views Product's app
+    """
+    fixtures = [
+        "fixtures/02-product-fixtures.json",
+    ]
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.product_list_view_data = {
+            "products": Product.objects.all(),
+            "url": reverse_lazy("products:products-list"),
+            "template": "products/product_list.html",
+        }
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_product_list_view(self):
+        """
+        Test class ProductListView response status code, template.
+        """
+        response: TemplateResponse = self.client.get(self.product_list_view_data["url"])
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, self.product_list_view_data["template"])
+
+    def test_product_list_view_correct_data(self):
+        """Test of correct display of data on page."""
+        response: TemplateResponse = self.client.get(self.product_list_view_data["url"])
+        self.assertEqual(list(self.product_list_view_data["products"]), list(response.context_data["products"]))
+
+
+class FormTestCase(TestCase):
+    ...
--- crm/users/tests.py
+++ crm/users/tests.py
@@ -5,6 +5,8 @@ from django.template.response import TemplateResponse
 from django.test import TestCase
 from django.urls import reverse_lazy
 
+from products.models import Product
+
 
 class UsersTestCase(TestCase):
     """
@@ -14,6 +16,10 @@ class UsersTestCase(TestCase):
     def setUpTestData(cls):
         cls.user = User.objects.create(username="mimia", password="momo123123")
         cls.index_page = reverse_lazy("users:index")
+        cls.index_view_context_data = {
+            "products_count": Product.objects.count()
+        }
+
     def setUp(self):
         self.client.force_login(self.user)
 
@@ -24,7 +30,12 @@ class UsersTestCase(TestCase):
         self.assertEqual(response.status_code, 302)
 
     def test_index_page_with_authentication(self):
-        """Test index page without authentication."""
+        """Test index page with authentication."""
         response: TemplateResponse = self.client.get(self.index_page)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.template_name[0], "users/index.html")
+
+    def test_correct_count_data(self):
+        """Test displaying correct data on pages"""
+        response: TemplateResponse = self.client.get(self.index_page)
+        self.assertEqual(response.context_data["products_count"], self.index_view_context_data["products_count"])
