0ec79cc3c7a12f146b09b0e81935f38c04b03632
Yaroslav D <dmiyaroslav92@gmail.com>
2024-07-26 18:46:08 -0600

    Added information about Product model into the index page

--- crm/users/views.py
+++ crm/users/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
+from products.models import Product
+
 
 class IndexPageView(LoginRequiredMixin, TemplateView):
     """
@@ -8,5 +10,8 @@ class IndexPageView(LoginRequiredMixin, TemplateView):
     """
     template_name = "users/index.html"
     login_url = "/admin/"
+
     def get_context_data(self, **kwargs):
-        ...
+        context = super().get_context_data(**kwargs)
+        context["products_count"] = Product.objects.count()
+        return context
