100662729016d6818c0fc6a902a74eb92685329d
Yaroslav D <dmiyaroslav92@gmail.com>
2024-07-28 21:43:36 -0600

    Added test for ProductForm.

--- crm/products/forms.py
+++ crm/products/forms.py
@@ -5,7 +5,7 @@ from .models import Product
 
 
 class ProductForm(ModelForm):
-    """Class for create  form for model Product"""
+    """Class for create  form for model Product."""
 
     class Meta:
         model = Product
@@ -16,9 +16,8 @@ class ProductForm(ModelForm):
         ]
 
     def clean_price(self):
-        print(self.cleaned_data)
+        """Check field price, have to be >= 0."""
         price = self.cleaned_data.get("price", "")
         if price < 0:
             raise ValidationError("Price must be a positive number", code="invalid")
-
         return price
--- crm/products/tests.py
+++ crm/products/tests.py
@@ -2,8 +2,8 @@ from django.template.response import TemplateResponse
 from django.test import TestCase, Client
 from django.urls import reverse_lazy
 
-from products.models import Product
-
+from .forms import ProductForm
+from .models import Product
 
 
 class ProductModelTestCase(TestCase):
@@ -12,7 +12,22 @@ class ProductModelTestCase(TestCase):
     """
     @classmethod
     def setUpTestData(cls):
-        ...
+        super().setUpTestData()
+        cls.product = Product.objects.create(name="test_name", description="test_description", price=10.50)
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        cls.product.delete()
+
+    def test_correct_creating_new_product(self):
+        """
+        Test of correct creation of a model object Product.
+        :return: None
+        """
+        expected_data = ["test_name", "test_description", 10.50]
+        new_obj_data = [self.product.name, self.product.description, self.product.price]
+        self.assertSequenceEqual(expected_data, new_obj_data)
 
 
 class ProductViewTestCase(TestCase):
@@ -38,16 +53,47 @@ class ProductViewTestCase(TestCase):
     def test_product_list_view(self):
         """
         Test class ProductListView response status code, template.
+        :return: None
         """
         response: TemplateResponse = self.client.get(self.product_list_view_data["url"])
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, self.product_list_view_data["template"])
 
     def test_product_list_view_correct_data(self):
-        """Test of correct display of data on page."""
+        """
+        Test of correct display of data on page.
+        :return: None
+        """
         response: TemplateResponse = self.client.get(self.product_list_view_data["url"])
         self.assertEqual(list(self.product_list_view_data["products"]), list(response.context_data["products"]))
 
 
 class FormTestCase(TestCase):
-    ...
+    """
+    Tests ProductForm form.
+    """
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.correct_data_form = {
+            "name": "test_name",
+            "description": "test_description",
+            "price": 20.88
+        }
+
+    def test_form_with_correct_data(self):
+        """
+        Test form with correct data and waited passing of form.is_valid().
+        :return: None
+        """
+        form = ProductForm(data=self.correct_data_form)
+        self.assertTrue(form.is_valid())
+
+    def test_price_with_incorrect_data(self):
+        """
+        Testing a form with incorrect data (price < 0) sent to it.
+        :return: None
+        """
+        self.correct_data_form["price"] = -1.99
+        form = ProductForm(data=self.correct_data_form)
+        self.assertFalse(form.is_valid())
