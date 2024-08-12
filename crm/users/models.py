30ece98a935182f3530a5e96e23176c64b6e9c00
Yaroslav D <dmiyaroslav92@gmail.com>
2024-08-11 19:40:13 -0600

    Should to think over if I need to have model UserModel.

--- crm/users/models.py
+++ crm/users/models.py
@@ -1,40 +1,40 @@
-from django.contrib.auth.models import User
-from django.core.validators import RegexValidator
-from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-phone_validator = RegexValidator(
-    regex=r'^\+?1?\d{9,15}$',
-    message="Phone number must be entered in the format '+123456789'. Up to 15 digits allowed."
-)
-
-
-class UserModel(models.Model):
-    """"Model DB User"""
-    USER_ROLES = [
-        ("AD", "Administrator"),
-        ("OP", "Operator"),
-        ("MR", "Marketer"),
-        ("MG", "Manager"),
-    ]
-    class Meta:
-        ordering = ["pk"]
-        verbose_name = "user"
-        verbose_name_plural = "users"
-
-    user: User = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=2, choices=USER_ROLES, verbose_name="role")
-    phone_number = models.CharField(max_length=16, blank=True, validators=[phone_validator,], verbose_name="phone")
-
-    @receiver(post_save, sender=User)
-    def create_user_model(sender, instance, created, **kwargs):
-        if created:
-            UserModel.objects.create(user=instance)
-
-    @receiver(post_save, sender=User)
-    def save_user_model(sender, instance, **kwargs):
-        instance.usermodel.save()
-
-    def __str__(self):
-        return self.user.username
+# from django.contrib.auth.models import User
+# from django.core.validators import RegexValidator
+# from django.db import models
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+#
+# phone_validator = RegexValidator(
+#     regex=r'^\+?1?\d{9,15}$',
+#     message="Phone number must be entered in the format '+123456789'. Up to 15 digits allowed."
+# )
+#
+#
+# class UserModel(models.Model):
+#     """"Model DB User"""
+#     USER_ROLES = [
+#         ("AD", "Administrator"),
+#         ("OP", "Operator"),
+#         ("MR", "Marketer"),
+#         ("MG", "Manager"),
+#     ]
+#     class Meta:
+#         ordering = ["pk"]
+#         verbose_name = "user"
+#         verbose_name_plural = "users"
+#
+#     user: User = models.OneToOneField(User, on_delete=models.CASCADE)
+#     role = models.CharField(max_length=2, choices=USER_ROLES, verbose_name="role")
+#     phone_number = models.CharField(max_length=16, blank=True, validators=[phone_validator,], verbose_name="phone")
+#
+#     @receiver(post_save, sender=User)
+#     def create_user_model(sender, instance, created, **kwargs):
+#         if created:
+#             UserModel.objects.create(user=instance)
+#
+#     @receiver(post_save, sender=User)
+#     def save_user_model(sender, instance, **kwargs):
+#         instance.usermodel.save()
+#
+#     def __str__(self):
+#         return self.user.username
